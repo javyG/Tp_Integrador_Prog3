f5ea681f6e990ca2f8ca10bd6c5101f1407d1bc7
romina-avari <92500421+romina-avari@users.noreply.github.com>
2023-08-16 19:46:10 -0300

    Form para actualizar datos (admi)
    Formulario para actualizar datos

--- ViajaYa.V-15-8/TP_Avances/formCambiar.php
+++ ViajaYa.V-15-8/TP_Avances/formCambiar.php
@@ -1,9 +1,10 @@
 <?php
 require_once('back-end/login.php');
 
-session_start();
+//session_start();
 
-if (isset($_SESSION['user']) && $_SESSION['user'] == true) {
+$_SESSION['user'] =  '<p>Sesion Iniciada</p>';
+if ($_SESSION) {
 
     $datos = new UsersModel();
     $registro = $datos->read();
@@ -32,45 +33,32 @@ if (isset($_SESSION['user']) && $_SESSION['user'] == true) {
         <div class="div1">
         </div>
 
-        <div class="div2">    
+        <div class="div2">
+            
+        <h4>Actulizacion de datos</h4>
+
                 <form class="formlogin" action="back-end/cambiar.php" method="post">
                 
-                <input type="hidden" name="userID" value="<?php echo $registro['user_id']?>"><br>
+                Id del usuario: <br>
+                <input type="text" name="user_ID" placeholder="Id"><br><br>
+                
+                Nuevo nombre: 
+                <input type="text" name="newname" placeholder="Nuevo nombre"><br><br>
+                
+                Nuevo correo electrónico: 
+                <input type="text" name="newmail" placeholder="Nuevo email"><br><br>
+                
+                Nuevo nombre usuario: 
+                <input type="text" name="newuser" placeholder="Nuevo usuario"><br><br>
+                
+                Nueva contraseña: 
+                <input type="text" name="newpass" placeholder="Nueva contraseña"><br><br>
 
-                <br>  Nuevo nombre: 
-                <input type="text" name="newname" placeholder="Nuevo nombre" value="<?php echo $registro['name']; ?>"><br>
+                <input type="submit" name="Cambiar" value="Cambiar" class="center"><br><br>
 
-                <br>  Nuevo correo: 
-                <input type="text" name="newmail" placeholder="Nuevo mail" value="<?php echo $registro['mail']; ?>"><br>
-
-                <br>  Nuevo nombre usuario: 
-                <input type="text" name="newuser" placeholder="Nuevo usuario" value="<?php echo $registro['user']; ?>"><br>
-
-                <br>  Nueva contraseña: 
-                <input type="text" name="newpass" placeholder="Nueva contraseña" value="<?php echo $registro['pass']; ?>"><br>
-
-                <br>  
-                <input type="submit" name="Cambiar" value="Cambiar" class="center"><br>
-
-                <br>
                 <a href="administrador.php"><input type="button" value="Cancelar" class="center"></a>
             </form> 
         </div>  
 </div>
 </section>
-
- </section>
-
- <footer>
-    <div class="piepag">
-      <h3 class="corpo"> Viaja Ya &copy; 2023 - Todos los derechos reservados   </h3>
-    </div>
- </footer> 
-</body>
-</html>
-
-<?php
-} else {
-    echo 'No ha iniciado sesion';
-}
 ?>
